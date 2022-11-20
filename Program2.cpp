5f0e41424ff827e83c5bcdbe7b8f9bb5177172c1
Rahul Kumar Bharti <97281881+rahulbharti6@users.noreply.github.com>
2022-11-20 20:25:17 +0530

    Init
    Write a program to write 10 first number

---
+++ Program2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  //For Loop
+  for (int i = 0; i <= 10; i = i + 1) {
+    cout << i << "\n";
+  }
+  //while Lopp
+  int i = 0;
+  while (i <= 10) {
+    cout << i << "\n";
+    i++;
+  }
+  return 0;
+}
